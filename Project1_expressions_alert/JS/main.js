f8ea5941711d7fe9d053f129bce074875301f369
Alan-Mitchell <94767396+Alan-Mitchell@users.noreply.github.com>
2021-12-18 20:54:35 -0600

    Update

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,7 @@
+window.alert("this is a test, it is only a test");//This line is an example of window alert. 
+document.write("This is to use document write method")//This line is an example of document.write, similar to the print function in HTML
+var A = "This is an example of" + "using concatenate"
+document.write(A)
+var N = "Ready or Not"
+var N = N.fontcolor("red")
+document.write(N)
