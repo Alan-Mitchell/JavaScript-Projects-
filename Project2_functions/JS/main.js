87480baa64234fd4349675e230a11075b97f7d73
Alan-Mitchell <94767396+Alan-Mitchell@users.noreply.github.com>
2021-12-19 23:40:43 -0600

    Update

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function MyFunction(){//This line represernts the keyowrd, naminng of the function, also the parameters are present.
+    var sentence = "I am learning";//This line is giving value to our variable sentence
+    sentence += "alot from this coding bootcamp";//This line is adding to our variable by using =+
+    document.getElementById("Blue").innerHTML=sentence;//this tells my html to change
+}
